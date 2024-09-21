b0aecc98e1d679a9e69edf6949938cc7e9325a44
= <=>
2024-09-21 14:41:43 +0330

    feat: connect to the postgres database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'nestjs-blog',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '9610',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
