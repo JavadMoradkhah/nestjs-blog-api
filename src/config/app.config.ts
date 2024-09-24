ab155c039b71605a87bbd8fc2edecf5dd140bd00
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-09-24 17:30:00 +0330

    refactor: change the default environment to development in the app config module

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 import { AppEnvironment } from 'src/common/enums/app-environment.enum';
 
 export default registerAs('app', () => ({
-  environment: process.env.NODE_ENV ?? AppEnvironment.PRODUCTION,
+  environment: process.env.NODE_ENV ?? AppEnvironment.DEVELOPMENT,
 }));
